064e8f3777ef50dd87ab0d9571c1dc221a96c658
tony2b2 <tonyaa2007@gmail.com>
2015-05-15 16:57:26 -0700

    Add method to add to the schedule window
    Now the class can be called with a method addStudentToSchedule to add a
    student to the schedule tab.

--- TAscheduler/src/GUI/configuration/ConfigurationWindow.java
+++ TAscheduler/src/GUI/configuration/ConfigurationWindow.java
@@ -16,7 +16,8 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Spinner;
-import org.eclipse.wb.swt.SWTResourceManager;
+//import org.eclipse.wb.swt.SWTResourceManager;
+
 
 public class ConfigurationWindow extends Shell {
 	private Table table;
